4646e68a269ff57648eac87b501f1a42675aaa26
José Adolfo Galdámez <josegalre@gmail.com>
2014-03-28 09:14:11 -0600

    Add fix for stupid preferred preview size bug

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -120,6 +120,10 @@ static char * camera_fixup_getparams(int id, const char * settings)
     params.remove(android::CameraParameters::KEY_PREFERRED_PREVIEW_SIZE_FOR_VIDEO);
 #endif
 
+#ifdef VIDEO_PREVIEW_ALWAYS_MAX
+     params.set(android::CameraParameters::KEY_PREFERRED_PREVIEW_SIZE_FOR_VIDEO, "1920x1080");
+#endif
+
 #ifdef DISABLE_FACE_DETECTION
 #ifndef DISABLE_FACE_DETECTION_BOTH_CAMERAS
     /* Disable face detection for front facing camera */
