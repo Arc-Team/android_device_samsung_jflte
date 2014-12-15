ff80251d177ce3db3892dd85a685c7c4b7e05505
dcd <dcd1182@gmail.com>
2014-12-15 17:45:38 -0600

    jf: fix some more denials
    Change-Id: I22683a332948ca8f16d830d76a315868275562d9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -190,20 +190,23 @@ BOARD_SEPOLICY_DIRS += device/samsung/jfltetmo/sepolicy
 BOARD_SEPOLICY_UNION += \
     bluetooth.te \
     device.te \
-    file.te \
     file_contexts \
+    file.te \
+    gamma_dev.te \
     genfs_contexts \
     healthd.te \
+    hostapd.te \
     insthk.te \
     kernel.te \
     keypad_dev.te \
     mdm_helper.te \
+    mediaserver.te \
     mm-pp-daemon.te \
     mm-qcamerad.te \
     mpdecision.te \
     panel_dev.te \
-    property.te \
     property_contexts \
+    property.te \
     rild.te \
     system_app.te \
     system_server.te \
