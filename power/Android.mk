096361a32c098c8c3c38960acbdfc7331bd840d9
Ricardo Cerqueira <ricardo@cyngn.com>
2014-12-23 00:42:32 +0000

    Fix multiarch builds
    Change-Id: Ib8ef16d4801666da591864ca72b381df34355e95

--- power/Android.mk
+++ power/Android.mk
@@ -3,11 +3,12 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_POWERHAL_VARIANT),jfltetmo)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE           := power.msm8960
-LOCAL_MODULE_PATH      := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
-LOCAL_MODULE_TAGS      := optional
-LOCAL_SHARED_LIBRARIES := liblog libcutils libdl
-LOCAL_SRC_FILES        := power.c metadata-parser.c utils.c list.c hint-data.c power-8960.c
+LOCAL_MODULE               := power.msm8960
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE_TAGS          := optional
+LOCAL_PROPRIETARY_MODULE   := true
+LOCAL_SHARED_LIBRARIES     := liblog libcutils libdl
+LOCAL_SRC_FILES            := power.c metadata-parser.c utils.c list.c hint-data.c power-8960.c
 include $(BUILD_SHARED_LIBRARY)
 
 endif
