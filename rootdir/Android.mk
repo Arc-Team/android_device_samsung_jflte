10bc6a58310d77b18b17f017d8146dfaa83a6f4d
Dan Pasanen <dan.pasanen@gmail.com>
2015-01-12 22:06:05 -0600

    Revert "jf: enable pulsing led while booting"
    This reverts commit 34b9087ddee8ae0972a72a6575afe19db4bc2914
    
    Change-Id: I31f818a42dc5cbc48d4d76e1570c0550189a16b8

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -54,10 +54,3 @@ LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.crda.sh
 include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.led.sh
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := init.led.sh
-include $(BUILD_PREBUILT)
