717f78ee54e3e7bfb18592283915bda545803bf0
dcd <dcd1182@gmail.com>
2015-08-02 16:19:25 -0500

    jf: move and update power management
    * move power to its own init
    
    * use healthd for charger
    
    * clean up init.qcom.rc
    
    * update charging mode flags and prop
    
    Change-Id: If02dc456f70468710b2419a8377747de0d2df142

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -17,6 +17,14 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.jfltetmo.power.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.jfltetmo.power.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.jfltetmo.usb.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
