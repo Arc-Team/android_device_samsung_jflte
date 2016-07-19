28562a9297b54e49bb01295abc185da079dd3212
Paul Keith <javelinanddart@gmail.com>
2016-07-19 11:04:18 -0700

    Update fingerprint
    * OK3 is the latest, not NK2
    
    Change-Id: I8ec7b5f4c771ad3fb3165164fe41c2b7fc9be46f
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- pac.mk
+++ pac.mk
@@ -24,5 +24,5 @@ PRODUCT_MANUFACTURER := Samsung
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=jfltetmo \
     TARGET_DEVICE=jfltetmo \
-    PRIVATE_BUILD_DESC="jfltetmo-user 4.4.4 KTU84P M919UVUFNK2 release-keys" \
-    BUILD_FINGERPRINT=samsung/jfltetmo/jfltetmo:4.4.4/KTU84P/M919UVUFNK2:user/release-keys
+    PRIVATE_BUILD_DESC="jfltetmo-user 4.4.4 KTU84P M919UVUFOK3 release-keys" \
+    BUILD_FINGERPRINT=samsung/jfltetmo/jfltetmo:4.4.4/KTU84P/M919UVUFOK3:user/release-keys
